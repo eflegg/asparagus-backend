907131cfbcb8fd01bf591d43836c930170b1895d
Erin Flegg <ehflegg@gmail.com>
2022-06-30 15:50:38 -0700

    update custom menu endpoint

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ add_action( 'rest_api_init', function () {
     /* Setup siteurl/wp-json/menus/v2/header */
 	register_rest_route( 'menus/v2', '/header', array(
 		'methods' => 'GET',
-		'callback' => 'header_menu',
+		'callback' => 'header_menu', 
 		'args' => array(
 			'id' => array(
 				'validate_callback' => function($param, $request, $key) {
@@ -49,6 +49,40 @@ add_action( 'rest_api_init', function () {
 		)
 	) );
 } );
+
+
+/* Register function to run at rest_api_init hook */
+add_action( 'rest_api_init', function () {
+    /* Setup siteurl/wp-json/menus/v2/footer */
+	register_rest_route( 'menus/v2', '/footer', array(
+		'methods' => 'GET',
+		'callback' => 'footer_menu', 
+		'args' => array(
+			'id' => array(
+				'validate_callback' => function($param, $request, $key) {
+					return is_numeric( $param );
+				}
+			),
+		)
+	) );
+} );
+
+/* Register function to run at rest_api_init hook */
+add_action( 'rest_api_init', function () {
+    /* Setup siteurl/wp-json/menus/v2/connect */
+	register_rest_route( 'menus/v2', '/connect', array(
+		'methods' => 'GET',
+		'callback' => 'connect_menu', 
+		'args' => array(
+			'id' => array(
+				'validate_callback' => function($param, $request, $key) {
+					return is_numeric( $param );
+				}
+			),
+		)
+	) );
+} );
+
 
 function header_menu( $data ) {
     /* Verify that menu locations are available in your WordPress site */
@@ -75,7 +109,8 @@ function header_menu( $data ) {
                         array_push(
                             $menuItems, array(
                                 'title' => $menu_item->title,
-                                'url' => $menu_item->url
+                                'url' => $menu_item->url,
+                                'slug' => $menu_item->slug,
                             )
                         );
                     }
@@ -141,4 +176,52 @@ function footer_menu( $data ) {
 
     /* Return array of list items with title and url properties */
 	return $footerMenuItems;
+}
+
+
+
+function connect_menu( $data ) {
+    /* Verify that menu locations are available in your WordPress site */
+    if (($locations = get_nav_menu_locations()) && isset($locations[ 'connect-menu' ])) {
+
+    /* Retrieve the menu in location header-menu */
+    $connectMenu = wp_get_nav_menu_object($locations['connect-menu']);
+
+    /* Create an empty array to store our JSON */
+    $connectMenuItems = array();
+
+    /* If the menu isn't empty, start process of building an array, otherwise return a 404 error */
+    if (!empty($connectMenu)) {
+
+        /* Assign array of navigation items to $menu_items variable */
+        $connect_menu_items = wp_get_nav_menu_items($connectMenu->term_id);
+
+            /* if $menu_items isn't empty */
+            if ($connect_menu_items) {
+
+                /* for each menu item, verify the menu item has no parent and then push the menu item to the $menuItems array */
+                foreach ($connect_menu_items as $key => $connect_menu_item) {
+                    if ($connect_menu_item->menu_item_parent == 0) {
+                        array_push(
+                            $connectMenuItems, array(
+                                'title' => $connect_menu_item->title,
+                                'url' => $connect_menu_item->url
+                            )
+                        );
+                    }
+                }
+            }
+        }
+    } else {
+        return new WP_Error(
+            'no_menus',
+            'Could not find any menus',
+            array(
+                'status' => 404
+            )
+        );
+    }
+
+    /* Return array of list items with title and url properties */
+	return $connectMenuItems;
 }
