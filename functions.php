52dca577137b7e4a3da031c05bf940b42ed7a557
Erin Flegg <ehflegg@gmail.com>
2022-07-15 08:50:59 -0700

    add cors handling function

--- functions.php
+++ functions.php
@@ -32,6 +32,8 @@ require_once 'inc/acf-options.php';
 // Add GraphQL resolvers.
 require_once 'inc/graphql/resolvers.php';
 
+// Enable featured image
+add_theme_support( 'post-thumbnails' );
 
 /**
  * Enable REST API support for the ad post type
@@ -42,3 +44,71 @@ add_filter( 'advanced-ads-post-type-params', function( $post_type_params ) {
 
     return $post_type_params;
 } );
+
+
+//Enable filtering ACF NOT WORKING
+
+add_filter( 'rest_contributor_query', function( $args ) {
+
+    $filters = [
+         'relation' => 'AND',
+    ];
+
+    foreach($_GET as $key => $value){
+         $filter = [
+              'key' => $key,
+              'value' => $value,
+              'compare' => '='
+         ];
+         array_push($filters, $filter);
+    }
+
+    $args['meta_query'] = $filters;
+
+    return $args;
+} );
+
+
+add_filter( 'rest_query_vars', function ( $valid_vars ) {
+    return array_merge( $valid_vars, array( 'writer', 'meta_query' ) );
+} );
+add_filter( 'rest_contributor_query', function( $args, $request ) {
+    $writer   = $request->get_param( 'writer' );
+
+    if ( ! empty( $highlight ) ) {
+        $args['meta_query'] = array(
+            array(
+                'key'     => 'writer',
+                'value'   => $writer,
+                'compare' => '=',
+            )
+        );      
+    }
+
+    return $args;
+}, 10, 2 );
+
+//Override temp CORS issues until frontend and backend domains match
+add_action('init', 'handle_preflight');
+function handle_preflight() {
+    $origin = get_http_origin();
+    if ($origin === 'http://locahost:3000') {
+        header("Access-Control-Allow-Origin: locahost:3000");
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Credentials: true");
+        header('Access-Control-Allow-Headers: Origin, X-Requested-With, X-WP-Nonce, Content-Type, Accept, Authorization');
+        if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
+            status_header(200);
+            exit();
+        }
+    }
+}
+add_filter('rest_authentication_errors', 'rest_filter_incoming_connections');
+function rest_filter_incoming_connections($errors) {
+    $request_server = $_SERVER['REMOTE_ADDR'];
+    $origin = get_http_origin();
+    if ($origin !== 'http://locahost:3000') return new WP_Error('forbidden_access', $origin, array(
+        'status' => 403
+    ));
+    return $errors;
+}
